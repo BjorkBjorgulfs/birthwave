f4dc2744330c2b31a0cec6c929b5507fe6a3046b
BjorkBjorgulfs <bogga1387@gmail.com>
2024-04-29 00:29:25 +0000

    styled the workshop pages

--- src/app/(pages)/about/client-fetch/page.tsx
+++ src/app/(pages)/about/client-fetch/page.tsx
@@ -7,6 +7,7 @@
 'use client';
 import { AboutMe } from "@prisma/client";
 import { useEffect, useState } from "react";
+import Image from 'next/image';
 
 export default function Page() {
     const [aboutData, setAboutData] = useState<AboutMe[] | null>(null);
@@ -34,16 +35,28 @@ export default function Page() {
     };
 
     return (
-        <div>
+        <main>
+            <div className="w-full h-[600px] relative overflow-hidden">
+                <Image src="/womanWithDoula.jpg" alt="Pregnant Woman With Doula" layout="fill" objectFit="cover" objectPosition="center 85%"  sizes= 'width: "auto"'/>
+            </div>
+            {/*here I am using the map function to iterate over the data and display it on the page*/}
             {aboutData.map((person) => (
-                <div key={person.id}>
-                    <h1>{person.name}</h1>
-                    <p>{person.email}</p>
-                    <p>{person.phone}</p>
-                    <p>{person.address}</p>
-                    <p>{person.info}</p>
+                <div className='flex w-full h-screen justify-center items-center gap-8'>
+                    <div key={person.id} className="w-[700px] h-3/4 bg-custom-pink rounded-2xl p-10 overflow-hidden">
+                        <p className="text-lg leading-8 overflow-ellipsis">{person.info}</p>
+                    </div>
+                    <div className='flex flex-col justify-end relative h-3/4 '>
+                        <div className="absolute top-0 right-0 w-[180px] h-2/5 bg-custom-pink rounded-full overflow-hidden"></div>
+                    <div key={person.id} className="w-[350px] h-2/5 bg-custom-yellow rounded-2xl p-6 overflow-hidden gap-8">
+                            <h1 className='text-lg'>The basics</h1>
+                            <p className='text-base'>{person.name}</p>
+                            <p className='text-base'>Email: {person.email}</p>
+                            <p className='text-base'>Phone: {person.phone}</p>
+                            <p className='text-base'>Address: {person.address}</p>
+                        </div>
+                    </div>
                 </div>
             ))}
-        </div>
+        </main>
     );
 };
--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -6,6 +6,7 @@
 // You can see the data in devtools -> network -> doc -> about
 import { fetchAboutMe } from '../../../lib/data';
 import { AboutMe } from '@prisma/client';
+import Image from 'next/image';
 
 export default async function Page() {
     const about: AboutMe[] | null = await fetchAboutMe();
@@ -15,17 +16,28 @@ export default async function Page() {
     };
 
     return (
-        <div>
+        <main>
+            <div className="w-full h-[600px] relative overflow-hidden">
+                <Image src="/womanWithDoula.jpg" alt="Pregnant Woman With Doula" layout="fill" objectFit="cover" objectPosition="center 85%"  sizes= 'width: "auto"'/>
+            </div>
             {/*here I am using the map function to iterate over the data and display it on the page*/}
             {about.map((person) => (
-                <div key={person.id}>
-                    <h1>{person.name}</h1>
-                    <p>{person.email}</p>
-                    <p>{person.phone}</p>
-                    <p>{person.address}</p>
-                    <p>{person.info}</p>
+                <div className='flex w-full h-screen justify-center items-center gap-8'>
+                    <div key={person.id} className="w-[700px] h-3/4 bg-custom-pink rounded-2xl p-10 overflow-hidden">
+                        <p className="text-lg leading-8 overflow-ellipsis">{person.info}</p>
+                    </div>
+                    <div className='flex flex-col justify-end relative h-3/4 '>
+                        <div className="absolute top-0 right-0 w-[180px] h-2/5 bg-custom-pink rounded-full overflow-hidden"></div>
+                    <div key={person.id} className="w-[350px] h-2/5 bg-custom-yellow rounded-2xl p-6 overflow-hidden gap-8">
+                            <h1 className='text-lg'>The basics</h1>
+                            <p className='text-base'>{person.name}</p>
+                            <p className='text-base'>Email: {person.email}</p>
+                            <p className='text-base'>Phone: {person.phone}</p>
+                            <p className='text-base'>Address: {person.address}</p>
+                        </div>
+                    </div>
                 </div>
             ))}
-        </div>
+        </main>
     );
 };
